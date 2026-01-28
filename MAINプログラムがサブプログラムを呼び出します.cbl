50acd77622e1ff701c97e3f57f8b8776836c782b
minh-eng <elyz.thedev@gmail.com>
2026-01-28 17:13:04 +0700

    CLEAR

--- MAINプログラムがサブプログラムを呼び出します.cbl
+++ MAINプログラムがサブプログラムを呼び出します.cbl
@@ -13,24 +13,17 @@
 000000*        2026/01/21 :            プログラムの作成
 000000*/-------------------------------------------------------------/*        
 000000 ENVIRONMENT                     DIVISION.
-000000*
 000000 INPUT-OUTPUT                    SECTION.
-000000*
 000000 FILE-CONTROL.
 000000     SELECT INPUT-FILE ASSIGN    TO  'INPUT'
 000000         ORGANIZATION IS SEQUENTIAL
 000000         FILE STATUS  IS WS-IN-STATUS.
-000000*
 000000 DATA                            DIVISION.
-000000*
 000000 FILE                            SECTION.
-000000*
 000000 FD  INPUT-FILE
 000000     RECORDING MODE IS F
 000000     RECORD CONTAINS 11 CHARACTERS.
-000000*
 000000 01 INPUT-REC                    PIC X(11).
-000000*
 000000 WORKING-STORAGE                 SECTION.
 000000*/-------------------------------------------------------------/*
 000000*  ワークエリア                                                   
@@ -61,13 +54,11 @@
 000000*/-------------------------------------------------------------/*
 000000*
 000000     DISPLAY CST-START-PGM-MSG
-000000*
 000000     OPEN INPUT INPUT-FILE.
 000000     IF WS-IN-STATUS NOT = '00'
 000000         DISPLAY 'ERROR OPEN INPUT FILE, STATUS : ' WS-IN-STATUS
 000000         STOP RUN
 000000     END-IF.
-000000*
 000000     PERFORM UNTIL CST-EOF = 'Y'
 000000         READ INPUT-FILE
 000000             AT END
@@ -80,16 +71,13 @@
 000000                 CALL    'SPD9999SET' USING  LNK-PARAM-JCL
 000000         END-READ
 000000     END-PERFORM
-000000*
 000000     CLOSE INPUT-FILE.
 000000     IF WS-IN-STATUS NOT = '00'
 000000         DISPLAY 'ERROR CLOSE INPUT FILE, STATUS : ' WS-IN-STATUS
 000000         STOP RUN
 000000     END-IF.
-000000*
 000000     DISPLAY 'TOTAL RECORD : ' CST-REC-COUNT
 000000     DISPLAY CST-STOP-PGM-MSG
-000000*
 000000     STOP RUN.
 000000*/-------------------------------------------------------------/* 
 000000 END PROGRAM SPD9999DRV.
